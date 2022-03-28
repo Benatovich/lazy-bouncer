bd3869efa16a2a88f9d5fedab8a9fbe5550672f7
Benatovich <sambenatovich@gmail.com>
2022-03-28 18:20:45 -0400

    the 401 test is passing now

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,7 +49,7 @@ describe('lazy-bouncer routes', () => {
     });
   });
 
-  it.only('returns the current user', async () => {
+  it('returns the current user', async () => {
     const [agent, user] = await registerAndLogin();
     const me = await agent.get('/api/v1/users/me');
 
@@ -60,7 +60,7 @@ describe('lazy-bouncer routes', () => {
     });
   });
 
-  it('should return a 401 when signed out and listing all users', async () => {
+  it.only('should return a 401 when signed out and listing all users', async () => {
     const res = await request(app).get('/api/v1/users');
     // console.log('|res.body', res.body);
     expect(res.body).toEqual({
